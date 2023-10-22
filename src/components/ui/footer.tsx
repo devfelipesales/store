a14c2776ab76ecd9a2c64c2d65eb5b72387e75c9
Felipe Sales <felipefsp.dev@gmail.com>
2023-10-22 12:02:11 -0300

    feat: add Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-10 bg-accent py-3">
+      <div className="container text-center text-[0.75rem] opacity-75">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
