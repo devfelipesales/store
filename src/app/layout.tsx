95d599c6edbacea94bc3fbb65588b0659d497619
Felipe Sales <felipefsp.dev@gmail.com>
2023-10-22 20:44:39 -0300

    feat: ensure recommended products are shown on products details page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/ui/header";
 import { AuthProvider } from "@/providers/auth";
 import Footer from "@/components/ui/footer";
-
+import CartProvider from "@/providers/cart";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,9 +23,11 @@ export default function RootLayout({
         {/* Essas duas divs foram criadas como para deixar o footer sempre em baixo quando não tiver conteúdo na página principal */}
         <div className="flex h-full flex-col">
           <AuthProvider>
-            <Header />
-            <div className="flex-1">{children}</div>
-            <Footer />
+            <CartProvider>
+              <Header />
+              <div className="flex-1">{children}</div>
+              <Footer />
+            </CartProvider>
           </AuthProvider>
         </div>
       </body>
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,4 +1,9 @@
 import { prismaClient } from "@/lib/prisma";
+import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
+import SectionTitle from "@/components/ui/section-title";
+import ProductImages from "./components/product-images";
+import ProductInfo from "./components/product-info";
 
 interface ProductDetailsPageProps {
   params: {
@@ -13,15 +18,32 @@ const ProductDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
-  if (!product) {
-    return;
-  }
+  if (!product) return null;
 
   return (
-    <div>
-      <h1>{product?.name}</h1>
+    <div className="flex flex-col gap-8 pb-8">
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductInfo product={computeProductTotalPrice(product)} />
+
+      <div>
+        <SectionTitle>Produtos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
     </div>
   );
 };
