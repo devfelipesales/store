8c25403ce12a201905389913d68f3fd1a8cc0431
Felipe Sales <felipefsp.dev@gmail.com>
2023-10-23 21:28:30 -0300

    feat: add create Order server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -9,14 +9,27 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useSession } from "next-auth/react";
+import { createOrder } from "@/actions/order";
 
 const Cart = () => {
+  const { data } = useSession();
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
+    if (!data?.user) {
+      // TODO: redirecionar para o login
+      return;
+    }
+
+    // Criar pedido no banco
+    const order = await createOrder(products, (data?.user as any).id);
+
     const checkout = await createCheckout(products);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    // Criar pedido no banco
 
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,4 +12,15 @@ export const authOption: AuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
 };
